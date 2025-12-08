1bcf1579ddf69ff1d4e3769da506901e8d2e91a2
P0KI-7 <kmp111207@gmail.com>
2025-12-08 23:26:58 +0300

    Add Constructor of Library

---
+++ src/Library.cpp
@@ -0,0 +1,107 @@
+#include "Library.h"
+
+#include <fstream>
+#include <iostream>
+
+Library::Library(std::string dataFile) 
+    :dataFile(dataFile), books(), users()
+{
+    std::string line;
+    std::ifstream file(dataFile);
+
+    if(!file.is_open()){
+        std::cerr << "Error: Could not open file " << dataFile << std::endl;
+        return;
+    }
+
+    Book tempBook;
+    User tempUser;
+    bool readingBooks = true;
+
+    while (std::getline(file,line))
+    {
+        if (line.empty()){
+            continue;
+        }
+        if (line == "---USERS---") {
+            readingBooks = false;
+            continue;
+        }
+
+        if (readingBooks) {
+            if (line == "BOOK") {
+                if (!tempBook.getTitle().empty()){
+                    books.push_back(tempBook);
+                }
+                tempBook = Book();
+                continue;
+            }
+
+            size_t colonPos = line.find(":");
+            if (colonPos != std::string::npos){
+                std::string key = line.substr(0,colonPos);
+                std::string value = line.substr(colonPos+2);
+
+                if (key == "Title"){
+                    tempBook.getTitle() = value;
+                }
+                else if (key == "Author"){
+                    tempBook.getAuthor() = value; 
+                }
+                else if (key == "Year"){
+                    tempBook.getYear() = std::stoi(value);
+                }
+                else if (key == "ISBN"){
+                    tempBook.getIsbn() = value;
+                }
+                else if (key == "Available"){
+                    if (value == "yes")
+                        tempBook.getIsAvailable() = 1;
+                    else
+                        tempBook.getIsAvailable() = 0;
+                }
+                else if (key == "BorrowedBy"){
+                    tempBook.getBorrowedBy() = value;
+                }
+            }
+        }
+        else {
+            if (line == "USER"){
+                if (!tempUser.getName().empty()){
+                    users.push_back(tempUser);
+                }
+                tempUser = User();
+                continue;
+            }
+            
+            size_t colonPos = line.find(":");
+            if (colonPos != std::string::npos){
+                std::string key = line.substr(0,colonPos);
+                std::string value = line.substr(colonPos+2);
+
+                if (key == "Name"){
+                    tempUser.getName() = value;
+                }
+                else if (key == "UserID"){
+                    tempUser.getUserID() = value;
+                }
+                else if (key == "BorrowedBooks"){
+                    tempUser.getBorrowBooks().push_back(value);
+                }
+                else if (key == "MaxBooks"){
+                    tempUser.getMaxBooksAllowed() = std::stoi(value);
+                }
+            }
+        }
+    }
+
+    if (!tempBook.getTitle().empty()) {
+        books.push_back(tempBook);
+    }
+    
+    if (!tempUser.getName().empty()){
+        users.push_back(tempUser);
+    }
+    
+    file.close();
+}
